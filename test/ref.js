66e5f26bbac9770906a7e713691053b45be1b256
Mathias Buus <mathiasbuus@gmail.com>
2024-10-02 20:28:10 +0200

    add another failing test

--- test/ref.js
+++ test/ref.js
@@ -13,3 +13,17 @@ test('unref and a timer stays alive', async function (t) {
     }, 50)
   }
 })
+
+// must be last test!
+test('ref in callbacks are noops', async function (t) {
+  t.plan(1)
+
+  const timer = timers.setTimeout(run, 10)
+
+  function run () {
+    timer.ref()
+    const other = timers.setTimeout(() => t.fail('should not run'), 10_000)
+    other.unref()
+    t.pass('timer triggered')
+  }
+})
